d3ced5d94dfbb0ee0205f677e257a69fc1782676
jarierie <73062348+jarierie@users.noreply.github.com>
2026-02-07 17:03:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,14 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Sure ka na?",
+    "Lovekoooo pls?",
+    "Hindi na pwede magbago isip mo?",
+    "Talaga? Kahit konti?",
+    "Final na ba?",
+    "Talaga? Kahit konti?",
+    "Sigurado ka talaga?",
+    "masasad ako..."
 ];
 
 let messageIndex = 0;
